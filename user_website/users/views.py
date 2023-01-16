d1667344e61d52e0aea5d15ca5f317aa332b7aac
ShrooqAyman <shayman@students.iugaza.edu.ps>
2023-01-16 13:36:07 +0200

    update user website

--- user_website/users/views.py
+++ user_website/users/views.py
@@ -10,10 +10,10 @@ def register(request):
     if request.method== 'GET':
         return render(request, 'users/register.html',{'form':CustomUserCreationForm})
     elif request.method == 'POST':
-        form = CustomUserCreationForm(request.post)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect(request, reverse('dashboard'))
+            return redirect( reverse('dashboard'))
 
 
